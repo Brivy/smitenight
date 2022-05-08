1bd72027217fc4c5423168505d8df9b5bdce57c1
Brian van Yperen <brianyperen@gmail.com>
2022-05-08 11:08:47 +0200

    Add the item request and responses

---
+++ Smitenight/Smitenight.Domain/Clients/SmiteClient/Requests/ItemRequests/ItemsRequest.cs
@@ -0,0 +1,13 @@
+﻿using Smitenight.Domain.Enums.SmiteClient;
+
+namespace Smitenight.Domain.Clients.SmiteClient.Requests.ItemRequests
+{
+    public record class ItemsRequest(
+        int DeveloperId,
+        string MethodName,
+        string ResponseType,
+        string Signature,
+        string SessionId,
+        string CurrentDate,
+        LanguageCodeEnum LanguageCode) : SmiteClientRequest(DeveloperId, MethodName, ResponseType, Signature, SessionId, CurrentDate);
+}
