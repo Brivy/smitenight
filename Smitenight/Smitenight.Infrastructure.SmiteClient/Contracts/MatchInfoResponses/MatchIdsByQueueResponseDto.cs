1ae2123ce68b64e69fa1c327e62472d00028c339
Brivy <Brianyperen@gmail.com>
2022-05-20 07:46:48 +0200

    Added the responses

---
+++ Smitenight/Smitenight.Infrastructure.SmiteClient/Contracts/MatchInfoResponses/MatchIdsByQueueResponseDto.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Smitenight.Infrastructure.SmiteClient.Contracts.MatchInfoResponses
+{
+    public record class MatchIdsByQueueResponseDto
+    {
+        [JsonPropertyName("Active_Flag")]
+        public string ActiveFlag { get; init; }
+        [JsonPropertyName("Match")]
+        public string Match { get; init; }
+        [JsonPropertyName("ret_msg")]
+        public object RetMsg { get; init; }
+    }
+}
