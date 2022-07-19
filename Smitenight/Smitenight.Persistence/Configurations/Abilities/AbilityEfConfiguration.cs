ee231eba19831ab09f5e0a393710185fe386a689
Brian van Yperen <Brianyperen@gmail.com>
2022-07-19 19:26:19 +0200

    Initial models and migration (#21)

---
+++ Smitenight/Smitenight.Persistence/Configurations/Abilities/AbilityEfConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Smitenight.Domain.Models.Abilities;
+
+namespace Smitenight.Persistence.Configurations.Abilities
+{
+    public class AbilityEfConfiguration : IEntityTypeConfiguration<Ability>
+    {
+        public void Configure(EntityTypeBuilder<Ability> builder)
+        {
+            builder.ToTable("Abilities");
+
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Cooldown).IsRequired();
+            builder.Property(x => x.Cost).IsRequired();
+            builder.Property(x => x.Summary).IsRequired();
+            builder.Property(x => x.Url).IsRequired();
+
+            builder.HasOne(x => x.God)
+                .WithMany(x => x.Abilities)
+                .HasForeignKey(x => x.GodId)
+                .OnDelete(DeleteBehavior.Cascade)
+                .IsRequired();
+
+            builder.HasMany(x => x.AbilityRanks)
+                .WithOne(x => x.Ability)
+                .HasForeignKey(x => x.AbilityId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(x => x.AbilityTags)
+                .WithOne(x => x.Ability)
+                .HasForeignKey(x => x.AbilityId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
