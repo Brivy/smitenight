d225a04fcf5a8d0d765cacbf8c2653f84bc8e60b
Brian van Yperen <Brianyperen@gmail.com>
2022-08-13 20:38:20 +0200

    Installing blazorise (#38)
    * Installing blazorise
    
    * Progress.
    
    * Even more satisfaction now
    
    * Some test stuff
    
    * Finally, some proper styling
    
    * No more javascript checking for now

--- SmitenightApp/SmitenightApp.Client/Program.cs
+++ SmitenightApp/SmitenightApp.Client/Program.cs
@@ -1,3 +1,6 @@
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SmitenightApp.Client;
@@ -7,5 +10,13 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services
+    .AddBlazorise(options =>
+    {
+        options.Immediate = true;
+    })
+    .AddBootstrapProviders()
+    .AddFontAwesomeIcons();
 
 await builder.Build().RunAsync();
