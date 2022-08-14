0e50c333059731d195ddf0c6105ae1bdcee65907
Brian van Yperen <Brianyperen@gmail.com>
2022-08-14 14:41:47 +0200

    Added some dto's (#42)

---
+++ SmitenightApp/SmitenightApp.Domain/Contracts/Abilities/AbilityRankDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SmitenightApp.Domain.Contracts.Abilities
+{
+    public class AbilityRankDto
+    {
+        public string Description { get; set; } = null!;
+        public string Value { get; set; } = null!;
+
+        public AbilityDto? Ability { get; set; }
+    }
+}
---
+++ SmitenightApp/SmitenightApp.Domain/Contracts/Abilities/AbilityTagDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SmitenightApp.Domain.Contracts.Abilities
+{
+    public class AbilityTagDto
+    {
+        public string Description { get; set; } = null!;
+        public string Value { get; set; } = null!;
+
+        public AbilityDto? Ability { get; set; }
+    }
+}
