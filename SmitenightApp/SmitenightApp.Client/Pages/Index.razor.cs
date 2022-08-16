360a2c5875a50301b7c578119b8df822c6889633
Brivy <Brianyperen@gmail.com>
2022-08-16 17:59:06 +0200

    Splitting god bubbles

--- SmitenightApp/SmitenightApp.Client/Pages/Index.razor.cs
+++ SmitenightApp/SmitenightApp.Client/Pages/Index.razor.cs
@@ -14,7 +14,6 @@ namespace SmitenightApp.Client.Pages
         private string ButtonStyle { get; set; } = null!;
         private string PinStyle { get; set; } = null!;
         private string LoaderStyle { get; set; } = null!;
-        private List<string> GodBubbleStyles { get; set; } = new();
 
         private Validations SmitenightValidations { get; set; } = null!;
         private Alert SmitenightSuccessAlert { get; set; } = null!;
@@ -24,47 +23,7 @@ namespace SmitenightApp.Client.Pages
 
         private readonly SmitenightProcessModel _smitenightProcessModel = new();
 
-        [Inject] private IGodClient GodClient { get; set; } = null!;
         [Inject] private ISmitenightClient SmitenightClient { get; set; } = null!;
-
-        protected override async Task OnInitializedAsync()
-        {
-            var gods = await GodClient.ListHomeLoadingSkinsAsync();
-            if (!gods.IsSuccessStatusCode || gods.Response == null)
-            {
-                return;
-            }
-
-            var godSkinCollection = gods.Response.Where(x => !string.IsNullOrWhiteSpace(x.GodSkinUrl)).Select(x => x.GodSkinUrl).ToList();
-            var delay = 0;
-            var random = new Random();
-            var previousRandomLefts = new List<int>();
-            foreach (var godCard in godSkinCollection)
-            {
-                var randomLeft = random.Next(0, 80);
-                while (previousRandomLefts.Any(x => x - 2 <= randomLeft && x + 2 >= randomLeft))
-                {
-                    randomLeft = random.Next(0, 70);
-                }
-
-                previousRandomLefts.Add(randomLeft);
-                var randomTimer = random.Next(20, 50);
-                var randomTop = random.Next(50, 60);
-                var randomScale = random.Next(20, 40);
-                var styling = $"content:url(\"{godCard}\"); " +
-                              $"opacity: 0; " +
-                              $"-webkit-animation: godsAnimation {randomTimer}s linear {delay}s infinite; " +
-                              $"-moz-animation: godsAnimation {randomTimer}s linear {delay}s infinite; " +
-                              $"animation: godsAnimation {randomTimer}s linear {delay}s infinite; " +
-                              $"left: {randomLeft}%; " +
-                              $"top: {randomTop}%; " +
-                              $"-webkit-transform: scale(0.{randomScale}); " +
-                              $"-moz-transform: scale(0.{randomScale}); " +
-                              $"transform: scale(0.{randomScale});";
-                GodBubbleStyles.Add(styling);
-                delay += 2;
-            }
-        }
 
         private async Task StartSmitenightAsync(CancellationToken cancellationToken = default)
         {
